4bda87aa13a2ad3fc8b2a543eb60118691e8b87d
eleonoraronchini <eleonora.ronchini@outlook.it>
2024-12-04 17:02:23 +0100

    updates

---
+++ src/components/TvShows.jsx
@@ -0,0 +1,18 @@
+
+
+const TVShows = () => {
+  return (
+    <div>
+      <h1>TV Shows</h1>
+      <p>Here are some of the popular TV shows:</p>
+      <ul>
+        <li>Breaking Bad</li>
+        <li>Stranger Things</li>
+        <li>Game of Thrones</li>
+        <li>The Crown</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TVShows;
